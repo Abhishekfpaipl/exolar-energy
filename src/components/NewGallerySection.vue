6c3b6502aa2d3278920e7375f2d01ec152e4a7e9
Abhishek Negi <abhishek.fpaipl@gmail.com>
2024-11-16 16:36:31 +0530

    gallery and step section updated

--- src/components/NewGallerySection.vue
+++ src/components/NewGallerySection.vue
@@ -1,41 +1,49 @@
 <template>
-    <!-- <div>
-        <div ref="lightGallery" class="gallery-container">
-            <a v-for="(image, index) in images" :key="index" :href="image.src">
-          <img :src="image.src" :alt="image.alt" class="gallery-thumbnail" />
-        </a> 
-            <a href="/img/gallery/1.jpeg">
-                <img src="/img/gallery/1.jpeg" alt="image 1" class="gallery-thumbnail" />
-            </a>
-        </div>
-    </div> -->
     <div class="container my-5">
-        <div class="text-start">
-            <p class="text-muted mb-1 text-uppercase">Dental Solutions</p>
+        <div class="text-start mb-5">
+            <p class="text-muted mb-1 text-uppercase">Exolar Energy</p>
             <div class="d-flex justify-content-between align-items-center">
                 <h2 class="display-5 text-uppercase" style="color: var(--bg-primary);">{{ title }}</h2>
                 <router-link :to="route" style="color: var(--bg-primary);"
                     class="text-decoration-none text-uppercase">see more <i class="bi bi-arrow-right"></i></router-link>
             </div>
         </div>
-        <p class="display-5 mb-5"></p>
-        <div class="d-flex gap-2 justify-content-start  overflow-x-scroll" ref="lightGallery" id="scroll">
-            <a :href="image" class="" v-for="(image, index) in images" :key="index">
-                <img :alt="'img' + (index + 1)" :src="image" class="card-img-top" />
-            </a>
+        <div class="row align-items-center ">
+            <div class="col-md-6">
+                <img src="/img/gallery.jpg" class="w-100" alt="">
+            </div>
+            <div class="col-md-6 mt-5 mt-md-0">
+                <div id="carouselGalleryAutoplaying" class="carousel slide mb-4" data-bs-ride="carousel"
+                    data-bs-interval="5000">
+                    <div class="carousel-inner">
+                        <div v-for="(image, index) in images" :key="index"
+                            :class="['carousel-item', { active: index === 0 }]">
+                            <div class="d-md-flex justify-content-center align-items-center d-none">
+                                <img :src="image" class="d-block w-75" alt="Carousel Image" style="width: 100%;" />
+                            </div>
+                            <img :src="image" class="d-block d-md-none" alt="Carousel Image" style="width: 100%;" />
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselGalleryAutoplaying"
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselGalleryAutoplaying"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+
+            </div>
         </div>
+
+
     </div>
 </template>
 
 <script>
-import lightGallery from 'lightgallery';
-import 'lightgallery/css/lightgallery.css';
-import 'lightgallery/css/lg-thumbnail.css';
-import 'lightgallery/css/lg-zoom.css';
-
-// Plugins
-import lgThumbnail from 'lightgallery/plugins/thumbnail';
-import lgZoom from 'lightgallery/plugins/zoom';
 
 export default {
     name: 'NewGallerySection',
@@ -52,24 +60,6 @@ export default {
             type: String,
             default: '/gallery',
         }
-    },
-    mounted() {
-        this.initGallery();
-    },
-    beforeUnmount() {
-        if (this.lightGalleryInstance) {
-            this.lightGalleryInstance.destroy();
-        }
-    },
-    methods: {
-        initGallery() {
-            this.lightGalleryInstance = lightGallery(this.$refs.lightGallery, {
-                plugins: [lgThumbnail, lgZoom],
-                speed: 500,
-                thumbnail: true,
-                zoom: true,
-            });
-        },
     },
 };
 </script>
--- src/components/SolarInstallationSteps.vue
+++ src/components/SolarInstallationSteps.vue
@@ -10,22 +10,30 @@
                 <img src="/img/stepsImage.jpeg" alt="" class="w-100 sticky-image">
             </div>
             <div class="col-md-6 mt-5 mt-md-0">
-                <div class="row row-cols-1 g-2">
-                    <div class="col " v-for="(step, index) in steps" :key="index" v-observe>
-                        <div class="border p-3 h-100 w-100 d-flex align-items-center justify-content-start gap-2"
-                            style="background-color: #F3F8F3;">
-                            <div class="d-flex justify-content-center align-items-center border rounded-circle position-relative"
-                                style="min-width: 100px; min-height: 100px;">
-                                <div class="position-absolute end-0 bg-success text-white rounded-circle mx-auto d-flex align-items-center justify-content-center"
-                                    style="width: 40px; height: 40px; font-size: 1.2rem; top: -10%">
-                                    {{ index + 1 }}
+                <div class="row row-cols-1 g-2 accordion accordion-flush" id="accordionFlushExample">
+                    <div class="col accordion-item" v-for="(step, index) in steps" :key="index" v-observe>
+                        <h2 class="accordion-header">
+                            <div class="accordion-button collapsed border p-3 h-100 w-100 d-flex align-items-center justify-content-start gap-2"
+                                type="button" data-bs-toggle="collapse" :data-bs-target="'#flush-collapseOne' + index"
+                                aria-expanded="false" :aria-controls="'flush-collapseOne' + index"
+                                style="background-color: #F3F8F3;">
+                                <div class="d-flex justify-content-center align-items-center border rounded-circle position-relative"
+                                    style="min-width: 100px; min-height: 100px;">
+                                    <div class="position-absolute end-0 bg-success text-white rounded-circle mx-auto d-flex align-items-center justify-content-center"
+                                        style="width: 40px; height: 40px; font-size: 1.2rem; top: -10%">
+                                        {{ index + 1 }}
+                                    </div>
+                                    <div class="d-flex justify-content-center align-items-center bg-light p-3 rounded-circle "
+                                        style="width: 50px; height: 50px;">
+                                        <i :class="`bi ${step.icon} fs-1 text-success`"></i>
+                                    </div>
                                 </div>
-                                <div class="d-flex justify-content-center align-items-center bg-light p-3 rounded-circle "
-                                    style="width: 50px; height: 50px;">
-                                    <i :class="`bi ${step.icon} fs-1 text-success`"></i>
-                                </div>
+                                <p class="mb-0 fs-3 text-start ms-3">{{ step.title }}</p>
                             </div>
-                            <p class="mb-0 fs-3 text-start ms-3">{{ step.title }}</p>
+                        </h2>
+                        <div :id="'flush-collapseOne' + index" class="accordion-collapse collapse"
+                            data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body">{{ step.description }}</div>
                         </div>
                     </div>
                 </div>
@@ -40,29 +48,69 @@ export default {
     data() {
         return {
             steps: [
-                { title: "Consultancy", icon: "bi-sun" },
-                { title: "End to End Survey", icon: "bi-geo" },
-                { title: "Detailed Engineering Report", icon: "bi-file-earmark-text" },
-                { title: "Procurement & Liaisoning", icon: "bi-shop-window" },
-                // { title: "Installation & Commissioning", icon: "bi-cone-striped" },
-                { title: 'Desktop Simulation', icon: 'bi bi-display', },
-                { title: 'Site Visit', icon: 'bi bi-geo-alt-fill', },
-                { title: 'Validation & Design', icon: 'bi bi-diagram-3', },
-                { title: 'Proposal & Acceptance', icon: 'bi bi-file-earmark-check', },
-                { title: 'Installation & Testing', icon: 'bi bi-hammer', },
-                { title: 'Net Metering', icon: 'bi bi-receipt', }
-            ],
+                {
+                    title: "Consultancy",
+                    icon: "bi-sun",
+                    description: "Providing expert advice and strategic planning to guide your project from start to finish."
+                },
+                {
+                    title: "End to End Survey",
+                    icon: "bi-geo",
+                    description: "Conducting comprehensive surveys to assess site conditions and gather necessary data for design and implementation."
+                },
+                {
+                    title: "Detailed Engineering Report",
+                    icon: "bi-file-earmark-text",
+                    description: "Creating a detailed report that outlines the technical specifications, design plans, and necessary steps for successful execution."
+                },
+                {
+                    title: "Procurement & Liaisoning",
+                    icon: "bi-shop-window",
+                    description: "Managing the procurement process for required materials and liaising with suppliers and vendors to ensure timely delivery."
+                },
+                {
+                    title: "Desktop Simulation",
+                    icon: "bi-display",
+                    description: "Using advanced software to simulate the system's performance, ensuring optimal design and function before installation."
+                },
+                {
+                    title: "Site Visit",
+                    icon: "bi-geo-alt-fill",
+                    description: "Visiting the site to assess physical conditions, make necessary adjustments to designs, and ensure readiness for the next steps."
+                },
+                {
+                    title: "Validation & Design",
+                    icon: "bi-diagram-3",
+                    description: "Validating the initial designs with on-site data and refining the design to meet all technical and functional requirements."
+                },
+                {
+                    title: "Proposal & Acceptance",
+                    icon: "bi-file-earmark-check",
+                    description: "Submitting a comprehensive proposal outlining costs, timelines, and specifications, followed by client acceptance to move forward."
+                },
+                {
+                    title: "Installation & Testing",
+                    icon: "bi-hammer",
+                    description: "Carrying out the installation process and conducting thorough testing to ensure all systems are functional and meet quality standards."
+                },
+                {
+                    title: "Net Metering",
+                    icon: "bi-receipt",
+                    description: "Implementing net metering systems to track energy usage and optimize energy efficiency, enabling clients to monitor consumption and savings."
+                }
+            ]
+
         };
     },
 };
 </script>
 
-<style scoped> 
+<style scoped>
 @media (min-width: 768px) {
- 
+
     .sticky-image {
         position: sticky;
-        top: 10%; 
+        top: 10%;
     }
 }
 </style>
